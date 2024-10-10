d259a1755a3d91111246f6e84cabc3c1ea9ea0d4
Ridoy Hossain <findridoy@gmail.com>
2024-10-10 11:50:35 +0600

    add ability to set next available day in vaccine center

--- app/Models/VaccineCenter.php
+++ app/Models/VaccineCenter.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,5 +20,29 @@ class VaccineCenter extends Model
         return [
             "available_day" => "datetime:Y-m-d",
         ];
+    }
+
+    public function setNextAvailableDay(): Carbon
+    {
+        $baseDate = $this->availableDayIsAfterToday()
+            ? $this->available_day
+            : now();
+
+        $nextWeekDay = get_next_weekday($baseDate);
+        $this->available_day = $nextWeekDay;
+        $this->available_day_booked = 0;
+        $this->save();
+
+        return $nextWeekDay;
+    }
+
+    public function spaceAvailableForSchedule(): bool
+    {
+        return $this->daily_limit > $this->available_day_booked;
+    }
+
+    public function availableDayIsAfterToday(): bool
+    {
+        return $this->available_day->startOfDay()->gt(now()->startOfDay());
     }
 }
